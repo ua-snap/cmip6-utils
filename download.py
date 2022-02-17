b7afcbbead75f6a5cc0631119e94f407117963c6
kyleredilla <kmredilla@alaska.edu>
2022-02-17 12:17:07 -0900

    initial commit of functions for tracking, downloading, and extracting data

---
+++ download.py
@@ -0,0 +1,201 @@
+"""Functions for downloading CMIP6 data via the CDS API"""
+
+import logging
+from pathlib import Path
+from zipfile import ZipFile, BadZipFile
+import cdsapi
+import pandas as pd
+
+
+def run_retrieve(api_di, fp, log_fp):
+    """Wrapper for the cdsapi.Client.retrieve function
+    to run the download request for a given set of parameters.
+    
+    Args:
+        api_di (dict): dict of kwargs for the request arg of the 
+            retrieve function
+        fp (path-like): path to write downloaded data
+        log_fp (path-like): path to text file where download logging
+            messages should be written
+    
+    Returns:
+        out (str): path to download path on CDS if success, error 
+            message if the download failed
+    """
+    c = cdsapi.Client(progress=False)
+    # this allows the logging outputs to be written to a file
+    output_file_handler = logging.FileHandler(log_fp)
+    # set the format for logging messages (same as in cdsapi)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    output_file_handler.setFormatter(formatter)
+    c.logger.addHandler(output_file_handler)
+    c.logger.propagate = False
+    
+    try:
+        result = c.retrieve("projections-cmip6", api_di, fp)
+        out = result.location
+    except Exception as exc:
+        out = exc.args[0]
+    return out
+
+
+def download(row, skip_existing=True):
+    """Attempt to download the data specified by the options in a row
+    of the download tracker table
+    
+    Args:
+        row (pandas.core.series.Series): row of the tracking table containing 
+            information for running a retrieval request via the CDS API
+        skip_existing (bool): skip the download if it already exists in base_dir
+            
+    Returns:
+        updated row with success
+    """
+    if skip_existing:
+        if row["result"] == "pass":
+            return row
+        
+    api_di = {
+        "format": "zip",
+        "temporal_resolution": row["t_res"],
+        "experiment": row["scenario"],
+        "level": "single_levels",
+        "variable": row["variable"],
+        "model": row["model"],
+    }
+    # if bbox is not NaN then convert to list and use for "area" arg
+    if row["bbox"] != "":
+        api_di["area"] = eval(row["bbox"])
+    zip_fp = row["zip_path"]
+    out = run_retrieve(api_di, zip_fp, row["log_path"])
+    if "http" in out:
+        row["result"] = "pass"
+    else:
+        row["result"] = "fail"
+        row["fail_reason"] = out
+    return row
+
+
+def update_tracker(tracker_fp, raw_dir):
+    """Update the tracking table based on presence of files. 
+    May be executed repeatedly without change if there are no updates to be made.
+    
+    Args:
+        tracker_fp (path-like): path to download tracking table
+        raw_dir (pathlib.PosixPath): path to directory where raw CMIP6 data
+            file should be extracted to
+            
+    Returns:
+        Nothing, prints the number of records updated
+    """
+    df = pd.read_csv(tracker_fp, keep_default_na=False)
+    
+    # check if data present in scratch_dir but not in base_dir
+    for row_id, row in df.iterrows():
+        zip_fp = Path(row["zip_path"])
+        if zip_fp.exists():
+            try:
+                # try to open the zipfile to see if success
+                with ZipFile(zip_fp, "r") as zip_ref:
+                    fns = zip_ref.namelist()
+            except BadZipFile:
+                df.at[row_id, "result"] = "fail"
+                df.at[row_id, "fail_reason"] = "BadZipFile"
+                continue
+
+            if row["result"] == "":
+                # if we're this far then the download was a success
+                df.at[row_id, "result"] = "pass"
+            elif row["result"] == "fail":
+                # this should never happen and would indicate 
+                #  corrupt / partial zip or something weird
+                if df.at[row_id, "fail_reason"] == "":
+                    df.at[row_id, "fail_reason"] = "Good zip file, but failed?"
+                continue
+                
+            # should only be one netcdf in there
+            dest_dir = raw_dir.joinpath(row["data_group"])
+            expected_base_fp = dest_dir.joinpath([fn for fn in fns if ".nc" in fn][0])
+            base_fp = Path(row["base_path"])
+            if str(base_fp) != "":
+                #not sure if this check is necessary
+                if expected_base_fp == base_fp:
+                    if base_fp.exists():
+                        pass
+                    else:
+                        # don't need to save base_fp return in this case
+                        # _ = unzip(zip_fp, dest_dir)
+                        pass
+                elif expected_base_fp.exists():
+                    df.at[row_id, "base_path"] = expected_base_fp
+                else:
+                    # df.at[row_id, "base_path"] = unzip(zip_fp, dest_dir)
+                    pass
+            elif expected_base_fp.exists():
+                # zip present with data .nc file, and expected data
+                #  present in base_dir, set base_path
+                df.at[row_id, "base_path"] = expected_base_fp
+        else:
+            pass
+        
+    df.to_csv(tracker_fp, index=False)
+    return df
+
+
+def unzip(zip_fp, dest_dir):
+    """Extract contents of a zip file to a destination directory
+    
+    Args:
+        zip_fp (path-like): path to zip file
+        dest_dir (pathlib.PosixPath): path to destination directory to extract to
+        
+    Returns:
+        base_fp (pathlib.PosixPath): New path to data file in $BASE_DIR
+    """
+    with ZipFile(zip_fp, "r") as zip_ref:
+        fns = zip_ref.namelist()
+        zip_ref.extractall(dest_dir)
+    # should only be one netcdf in there
+    base_fp = dest_dir.joinpath([fn for fn in fns if ".nc" in fn][0])
+    return base_fp
+
+
+
+def batch_unzip(tracker_fp, raw_dir):
+    """Iterate over the rows of the tracker table and
+    attempt to extract any data files that have not been 
+    extracted yet
+    
+    Args:
+        tracker_fp (path-like): path to download tracking table
+        raw_dir (pathlib.PosixPath): path to directory where raw CMIP6 data
+            file should be extracted to
+        
+    Returns:
+        list of new data files extracted to base_dir
+    """
+    df = update_tracker(tracker_fp, raw_dir)
+    
+    new_base_fps = []
+    for row_id, row in df.iterrows():
+        zip_fp = Path(row["zip_path"])
+        if zip_fp.exists() and (row["fail_reason"] != "BadZipFile"):
+            with ZipFile(zip_fp, "r") as zip_ref:
+                fns = zip_ref.namelist()
+            dest_dir = raw_dir.joinpath(row["data_group"])
+            expected_base_fp = dest_dir.joinpath([fn for fn in fns if ".nc" in fn][0])
+            if not expected_base_fp.exists():
+                try:
+                    # unzip and update dataframe
+                    base_fp = unzip(zip_fp, dest_dir)
+                except BadZipFile as exc:
+                    df.at[row_id, "fail_reason"] = exc.args[0]
+                    continue
+                    
+                new_base_fps.append(base_fp)
+                df.at[row_id, "base_path"] = base_fp
+                
+    # save changes to tracker df
+    df.to_csv(tracker_fp, index=False)
+    print(f"{len(new_base_fps)} data files extracted from zips")
+    return df
