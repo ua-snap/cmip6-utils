58c1e3a1260c26a1a9ee594bf09cf0ae7b7a5491
kyleredilla <kmredilla@alaska.edu>
2023-03-16 13:00:08 -0800

    adds some utilites for authenticating and creating objects for use with the globus_sdk

--- transfers/utils.py
+++ transfers/utils.py
@@ -8,6 +8,131 @@ The general structure is retained: use a dict of {endpoint: [list, of, filepaths
 import re
 from pathlib import Path
 import subprocess
+import globus_sdk
+from globus_sdk.scopes import TransferScopes
+
+
+def check_transfer_token(token):
+    """Checks a transfer token for validity"""
+
+    
+def login_and_get_transfer_client(auth_client, scopes=TransferScopes.all):
+    """This forces a login each time but is a way to create a TransferClient easily.
+    
+    From globus_sdk docs: https://globus-sdk-python.readthedocs.io/en/stable/examples/minimal_transfer_script/index.html#example-minimal-transfer
+    """
+    # note that 'requested_scopes' can be a single scope or a list
+    # this did not matter in previous examples but will be leveraged in
+    # this one
+    auth_client.oauth2_start_flow(requested_scopes=scopes)
+    authorize_url = auth_client.oauth2_get_authorize_url()
+    print(f"Please go to this URL and login:\n\n{authorize_url}\n")
+
+    auth_code = input("Please enter the code here: ").strip()
+    tokens = auth_client.oauth2_exchange_code_for_tokens(auth_code)
+    transfer_tokens = tokens.by_resource_server["transfer.api.globus.org"]
+
+    # return the TransferClient object, as the result of doing a login
+    return globus_sdk.TransferClient(
+        authorizer=globus_sdk.AccessTokenAuthorizer(transfer_tokens["access_token"])
+    )
+
+
+def check_for_consent_required(tc, auth_client, target):
+    """Function to check if consent is required
+    
+    Modified from globus_sdk docs
+    """
+    # now, try an ls on the source and destination to see if ConsentRequired
+    # errors are raised
+    consent_required_scopes = []
+    try:
+        tc.operation_ls(target, path="/")
+    # catch all errors and discard those other than ConsentRequired
+    # e.g. ignore PermissionDenied errors as not relevant
+    except globus_sdk.TransferAPIError as err:
+        if err.info.consent_required:
+            consent_required_scopes.extend(err.info.consent_required.required_scopes)
+    
+    # handle ConsentRequired with a new login
+    if consent_required_scopes:
+        print(
+            "One of your endpoints requires consent in order to be used.\n"
+            "You must login a second time to grant consents.\n\n"
+        )
+        tc = login_and_get_transfer_client(auth_client, scopes=consent_required_scopes)
+        
+    return tc
+        
+    
+
+
+def make_globus_client():
+    """Make a globus client object. """
+    client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+    client.oauth2_start_flow()
+    
+    
+def authorize_client(client):
+    """Authorize the Globus client. Returns the tokens needed for using the globus_sdk package for transfers etc."""
+    authorize_url = client.oauth2_get_authorize_url()
+    print(f"Please go to this URL and login:\n\n{authorize_url}\n")
+
+    auth_code = input("Please enter the code you get after login here: ").strip()
+    token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+
+    globus_auth_data = token_response.by_resource_server["auth.globus.org"]
+    globus_transfer_data = token_response.by_resource_server["transfer.api.globus.org"]
+
+    # most specifically, you want these tokens as strings
+    AUTH_TOKEN = globus_auth_data["access_token"]
+    TRANSFER_TOKEN = globus_transfer_data["access_token"]
+    
+    return TRANSFER_TOKEN, AUTH_TOKEN
+
+
+def make_transfer_client(token):
+    """Make a TransferClient using a token. Will """
+    authorizer = globus_sdk.AccessTokenAuthorizer(token)
+    tc = globus_sdk.TransferClient(authorizer=authorizer)
+    
+    try:
+        r = tc.get_endpoint(config.acdn_ep)
+    except globus_sdk.TransferAPIError as exc:
+        if exc.http_status == 401:
+            # need to re-authorize
+            authorize_client()
+            
+    return token
+
+
+def write_token_info():
+    """"""
+
+
+def activate_endpoint(tc, ep):
+    """Activate an endpoint, and handle case where manual activation is required.
+    
+    Args:
+        tc (globus_sdk.services.transfer.client.TransferClient): transfer client object
+        ep (str): endpoint ID
+    
+    This borrowed from globus_sdk docs: https://globus-sdk-python.readthedocs.io/_/downloads/en/3.0.0a1/pdf/
+    """
+    # cannot autoactivate the ESGF endpoints, but can use the function to help get there
+    r = tc.endpoint_autoactivate(ep, if_expires_in=3600)
+    while (r["code"] == "AutoActivationFailed"):
+        print(
+            "Endpoint requires manual activation, please open "
+            "the following URL in a browser to activate the "
+            "endpoint:"
+            f"https://app.globus.org/file-manager?origin_id={ep}"
+        )
+        input("Press ENTER after activating the endpoint:")
+        r = tc.endpoint_autoactivate(ep, if_expires_in=3600)
+        
+    return
+
 
 def get_contents(ep, path):
     """Get the contents of a Globus directory as a list of string values"""
@@ -28,6 +153,27 @@ def get_contents(ep, path):
         return int(http_status)
         
     contents = [c for c in out.decode("utf-8").split("\n") if c != ""]
+    
+    return contents
+
+
+def operation_ls(tc, ep, path):
+    """ls operation on the specified endpoint:path. Wrapper for the globus_sdk.services.transfer.client.TransferClient.operation_ls function.
+    
+    Args:
+        tc (globus_sdk.services.transfer.client.TransferClient): transfer client object
+        ep (str): endpoint ID
+        path (str): path on the specified endpoint to run ls
+    
+    Returns:
+        contents (list): list of contents (as strings) in the directory or HTTP error code
+    """
+    try:
+        r = tc.operation_ls(ep, path)
+    except globus_sdk.TransferAPIError as exc:
+        return exc.http_status
+        
+    contents = [item["name"] for item in r]
     
     return contents
     
@@ -71,11 +217,6 @@ def arguments(argv):
         upath = '/' + upath
 
     return (username, upath, listonly)
-
-
-
-def generate_local_path(src_fp, base_dir):
-    """Create a path to save """
 
 
 def make_batch_file(ep_di, u_ep, username, base_dir, batch_fp):
