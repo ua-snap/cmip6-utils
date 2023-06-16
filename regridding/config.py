52dc7d02082719a7b5f6a5810747dee27bba6c15
Kyle <kmredilla@alaska.edu>
2023-06-16 09:21:15 -0800

    add more config variables for slurming

--- regridding/config.py
+++ regridding/config.py
@@ -4,12 +4,28 @@ import os
 from pathlib import Path
 
 
+# path-based required env vars will throw error if None
+# path to root of this repo, for constructing absolute paths to scripts
+PROJECT_DIR = Path(os.getenv("PROJECT_DIR"))
 SCRATCH_DIR = Path(os.getenv("SCRATCH_DIR"))
+conda_init_script = Path(os.getenv("CONDA_INIT"))
+try:
+    slurm_email = Path(os.getenv("SLURM_EMAIL"))
+except TypeError:
+    slurm_email = None
 
+# path to script for regridding
+regrid_script = PROJECT_DIR.joinpath("regridding/regrid.py")
+
+# slurm info (doesn't need to be hardcoded, but OK for now?)
+partition = "t1small"
+ncpus = 24
+
+# this will probably not change between users
 cmip6_dir = Path("/beegfs/CMIP6/arctic-cmip6/CMIP6")
 
 # manifest file
-manifest_fp = "../transfers/llnl_manifest.csv"
+manifest_fp = PROJECT_DIR.joinpath("transfers/llnl_manifest.csv")
 
 # directory to write re-gridded files
 regrid_dir = Path("/beegfs/CMIP6/arctic-cmip6/regrid")
