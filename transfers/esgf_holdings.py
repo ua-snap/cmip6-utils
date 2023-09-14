dc9d9535ae6578878eed3d8eab4b0364d6e3b0e8
Kyle <kmredilla@alaska.edu>
2023-09-14 14:06:32 -0800

    add autoactivate for endpoint

--- transfers/esgf_holdings.py
+++ transfers/esgf_holdings.py
@@ -34,47 +34,53 @@ def list_variants(tc, esgf_node, activity, model, scenario):
     scenario_path = node_prefix.joinpath(
         activity, luts.model_inst_lu[model], model, scenario
     )
-    
+
     variants = utils.operation_ls(tc, node_ep, scenario_path)
-    
+
     if isinstance(variants, int):
         return {}
     elif isinstance(variants, list):
         return {"model": model, "scenario": scenario, "variants": variants}
-    
-    
+
+
 def make_model_variants_lut(tc, esgf_node, models, scenarios):
-    """Create a lookup table of all variants available for each model/ scenario combination. Uses an existing TransferClient object. 
-    
+    """Create a lookup table of all variants available for each model/ scenario combination. Uses an existing TransferClient object.
+
     Returns a pandas DataFrame with a list of variants available for each model/scenario
     """
     # find what variants are available for each model / scenario combination
-    args = list(
-        product(["CMIP"], models, ["historical"])
-    ) + list(
+    args = list(product(["CMIP"], models, ["historical"])) + list(
         product(["ScenarioMIP"], models, scenarios)
     )
     # include the TransferClient object for each query
     args = [[tc, esgf_node] + list(a) for a in args]
-    
-    with Pool(32) as pool:
+
+    with Pool(12) as pool:
         rows = pool.starmap(list_variants, args)
-        
+
     df = pd.DataFrame(rows)
-    
+
     return df.dropna()
 
 
-def get_filenames(tc, esgf_node, activity, model, scenario, variant, frequency, varname):
+def get_filenames(
+    tc, esgf_node, activity, model, scenario, variant, frequency, varname
+):
     """Get the file names for a some combination of model, scenario, and variable."""
     node_ep = luts.globus_esgf_endpoints[esgf_node]["ep"]
     node_prefix = Path(luts.globus_esgf_endpoints[esgf_node]["prefix"])
-    
+
     # the subdirectory under the variable name is the grid type.
-    #  This is almost always "gn", meaning the model's native grid, but it could be different. 
+    #  This is almost always "gn", meaning the model's native grid, but it could be different.
     #  So we have to check it instead of assuming. I have only seen one model where this is different (gr1, GFDL-ESM4)
     var_path = node_prefix.joinpath(
-        activity, luts.model_inst_lu[model], model, scenario, variant, frequency, varname
+        activity,
+        luts.model_inst_lu[model],
+        model,
+        scenario,
+        variant,
+        frequency,
+        varname,
     )
     grid_type = utils.operation_ls(tc, node_ep, var_path)
 
@@ -111,7 +117,7 @@ def get_filenames(tc, esgf_node, activity, model, scenario, variant, frequency, 
             "n_files": len(fns),
             "filenames": fns,
         }
-    
+
     return row_di
 
 
@@ -124,14 +130,14 @@ def make_holdings_table(tc, esgf_node, models, scenarios, variant_lut, varnames)
         activity = "CMIP" if row["scenario"] == "historical" else "ScenarioMIP"
         args.extend(
             product(
-                [tc], 
-                [esgf_node], 
-                [activity], 
-                [row["model"]], 
-                [row["scenario"]], 
+                [tc],
+                [esgf_node],
+                [activity],
+                [row["model"]],
+                [row["scenario"]],
                 row["variants"],
-                freqs, 
-                varnames
+                freqs,
+                varnames,
             )
         )
 
@@ -139,30 +145,44 @@ def make_holdings_table(tc, esgf_node, models, scenarios, variant_lut, varnames)
         rows = pool.starmap(get_filenames, args)
 
     filenames_lu = pd.DataFrame(rows)
-    
+
     return filenames_lu
 
 
 if __name__ == "__main__":
     esgf_node = arguments(sys.argv)
-    
+
     # create an authorization client for Globus
     auth_client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
     tc = utils.login_and_get_transfer_client(auth_client)
-    
+
     print("Log in successful. Running the audit now.")
-    
+
     # check if we need to grant conesnt for ACDN
     utils.check_for_consent_required(tc, auth_client, acdn_ep)
-    
+
+    # I think this activation has to be done?
+    node_ep = luts.globus_esgf_endpoints[esgf_node]["ep"]
+    tc.endpoint_autoactivate(node_ep)
+
     # specify the models we are interested in
     # currently this is just everything in the luts.model_inst_lu table
     models = list(luts.model_inst_lu.keys())
-    
+
     # get a dataframe of variants available for each model and scenario
     variant_lut = make_model_variants_lut(tc, esgf_node, models, prod_scenarios)
-    
+
+    # Check that we won't get a particular error which pops up when the user has not logged into the ESGF node via Globus
+    try:
+        _ = variant_lut.iloc[0]["scenario"]
+    except KeyError:
+        print(
+            "Key error. Check that you have logged into the endpoint via the Globus app."
+        )
+
     # amke the holdings table
-    holdings_df = make_holdings_table(tc, esgf_node, models, prod_scenarios, variant_lut, prod_vars)
+    holdings_df = make_holdings_table(
+        tc, esgf_node, models, prod_scenarios, variant_lut, prod_vars
+    )
 
     holdings_df.to_csv(f"{esgf_node}_esgf_holdings.csv", index=False)
