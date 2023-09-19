9d466778d5b732c51c840112cca70000a9c472ff
Kyle <kmredilla@alaska.edu>
2023-09-19 12:43:01 -0800

    fix to include all day and mon freq types in transfer

--- transfers/transfer.py
+++ transfers/transfer.py
@@ -25,7 +25,7 @@ def arguments(argv):
         "-f",
         "--freq",
         type=str,
-        help="Temporal frequency (currently only option is 'day' and this is default)",
+        help="Temporal frequency. Either 'day' (default) or 'mon'.",
         default="day",
     )
     parser.add_argument(
@@ -35,15 +35,16 @@ def arguments(argv):
         help="Set the --dry-run switch in globus transfer",
     )
     args = parser.parse_args()
-    
+
     return args.varname, args.freq, args.dry_run
 
-                                     
+
 if __name__ == "__main__":
     varname, freq, dry_run = arguments(sys.argv)
-    
+
     if varname == "all_variables":
-        batch_fps = Path("batch_files").glob(f"batch_llnl_{freq}_*.txt")
+        # wildcard in front of frequency to handle different ones like day, Oday, SIday etc
+        batch_fps = Path("batch_files").glob(f"batch_llnl_*{freq}_*.txt")
         all_fp = Path(f"/tmp/batch_llnl_{freq}_all_variables.txt")
         batch_fp = str(all_fp)
         with open(batch_fp, "w") as batch_file:
@@ -52,7 +53,7 @@ if __name__ == "__main__":
                     batch_file.write(infile.read())
     else:
         batch_fp = f"batch_files/batch_llnl_{freq}_{varname}.txt"
-    
+
     command = [
         "globus",
         "transfer",
@@ -65,17 +66,15 @@ if __name__ == "__main__":
         "--sync-level",
         "mtime",
     ]
-    
+
     if dry_run:
         command += ["--dry-run"]
-    
+
     out = check_output(command)
     print(out.decode("utf-8"))
-    
+
     # delete the all-variable batch file if created
     try:
         all_fp.unlink()
     except:
         pass
-    
-                                     
