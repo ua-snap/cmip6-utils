fec87655214f49af826bc75c9989cfa1a487af27
Joshdpaul <jdpaul3@alaska.edu>
2024-01-17 12:26:35 -0900

    new branch to add test for time dimension before regridding

---
+++ regridding/tests/time_dim_test.py
@@ -0,0 +1,35 @@
+import xarray as xr
+import glob
+import warnings
+from multiprocessing import Pool
+from config.py import PROJECT_DIR, cmip6_dir
+
+warnings.simplefilter("ignore")
+
+#define csv file for logging (this is the blank csv created in the check_time_dim.slurm script)
+time_dim_error_file = PROJECT_DIR.joinpath('regridding/tests/time_dim_test.csv')
+#list all *.nc files in the CMIP6 directory
+files = glob.glob(cmip6_dir.joinpath('**/*.nc'), recursive = True)
+
+#all files passing transfers/tests.slurm should be readable with xarray
+#this test assumes they are readable and tests for a time dimension
+#files without a time dimension return their filepath as a result
+#files with a time dimension will return None
+def test_nc_file(file):
+    
+    try:
+        ds = xr.open_dataset(file)
+        t = ds.time
+    except:
+        return file
+
+if __name__ == "__main__":
+
+    with Pool() as pool:
+        results = pool.map(test_nc_file, files)
+        
+    # sort non-None results and write filepaths to csv
+    for r in results:
+        if r is not None:
+            with open(time_dim_error_file, 'a') as f:
+                f.write(str(r + '\n'))
