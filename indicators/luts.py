1f895d7a54b699fd9ed490837bc4751a034c3e46
Kyle <kmredilla@alaska.edu>
2024-01-18 10:35:16 -0900

    Add core indicators processing script

--- indicators/luts.py
+++ indicators/luts.py
@@ -1,4 +1,5 @@
 # map from model variable names to possible index variable names
+# helps with possible optimization of computing indicators that rely on the same datasets
 varid_idx_lu = {
     # "pr": ["rx1day", "rx5day", "r10mm", "cwd", "cdd"],
     "pr": ["rx1day", "rx5day", "r10mm", "cwd", "cdd"],
@@ -6,4 +7,13 @@ varid_idx_lu = {
     "tasmax": ["hd", "su", "wsdi"],
     "tasmin": ["cd", "dw", "csdi"],
     # "sfcWind": ["wndd"],
+}
+
+# this is the reverse lookup, for mapping indicators to the list of CMIP6 variable ID's that are needed
+idx_varid_lu = {
+    "su": ["tasmax"],
+    "su2": ["tasmax"],
+    "dw": ["tasmin"],
+    "ftc": ["tasmax", "tasmin"],
+    "rx1day": ["pr"],
 }
