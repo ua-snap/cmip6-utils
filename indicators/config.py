6a45e6f90c19abb1711224e826091184fd24f023
Bob Torgerson <rltorgerson@alaska.edu>
2024-01-22 14:47:51 -0900

    Adds indicator Slurm sbatch generation to the cmip6-utils functionality (#9)
    * Interim work to generate Slurm scripts from Prefect parameters.
    
    * Fixes case where env var has not been set and uses default.
    
    * Adds missing parameter.
    
    * Adds required backup dir to function call.
    
    * Fixes input spacing.
    
    * Adds end time to script.
    
    * Submits jobs when they have been generated.
    
    * Adds TODO for having indicators that use the same data be run all together.
    
    * use env var for slurm in indicators/config.py
    
    * Sets defaults to user snapdata user for running the indicator generation.
    
    * Updates scratch directory with correct file path.
    
    ---------
    
    Co-authored-by: Kyle Redilla <kmredilla@alaska.edu>

--- indicators/config.py
+++ indicators/config.py
@@ -6,15 +6,40 @@ from pathlib import Path
 
 # path-based required env vars will throw error if None
 # path to root of this repo, for constructing absolute paths to scripts
-# PROJECT_DIR = Path(os.getenv("PROJECT_DIR"))
-# SCRATCH_DIR = Path(os.getenv("SCRATCH_DIR"))
-conda_init_script = Path(os.getenv("CONDA_INIT"))
+PROJECT_DIR = (
+    Path(os.getenv("PROJECT_DIR"))
+    if "PROJECT_DIR" in os.environ
+    else Path("/home/snapdata/cmip6-utils/")
+)
+SCRATCH_DIR = (
+    Path(os.getenv("SCRATCH_DIR"))
+    if "SCRATCH_DIR" in os.environ
+    else Path("/beegfs/CMIP6/snapdata/")
+)
+conda_init_script = (
+    Path(os.getenv("CONDA_INIT"))
+    if "CONDA_INIT" in os.environ
+    else PROJECT_DIR.joinpath("indicators/conda_init.sh")
+)
 try:
     slurm_email = Path(os.getenv("SLURM_EMAIL"))
 except TypeError:
     slurm_email = None
 
 # this will probably not change between users
-cmip6_dir = Path("/beegfs/CMIP6/arctic-cmip6/CMIP6")
+cmip6_dir = Path("/beegfs/CMIP6/arctic-cmip6/")
+regrid_dir = Path("/beegfs/CMIP6/arctic-cmip6/regrid/")
+slurm_dir = SCRATCH_DIR.joinpath("slurm")
+
+# path to script for regridding
+indicators_script = PROJECT_DIR.joinpath("indicators/indicators.py")
 
 indicator_tmp_fp = "{indicator}_{model}_{scenario}_indicator.nc"
+
+sbatch_head_kwargs = {
+    # slurm info (doesn't need to be hardcoded, but OK for now?)
+    "partition": "t2small",
+    "ncpus": 24,
+    "conda_init_script": conda_init_script,
+    "slurm_email": slurm_email,
+}
