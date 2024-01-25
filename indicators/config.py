006331d70e659bb2e943f31f2607ab8593ddfba2
Bob Torgerson <rltorgerson@alaska.edu>
2024-01-25 12:51:28 -0900

    Modifies code to use input parameters to set directory structure.

--- indicators/config.py
+++ indicators/config.py
@@ -1,45 +1,8 @@
 """Configuration for CMIP6 regridding"""
 
-import os
 from pathlib import Path
 
-
-# path-based required env vars will throw error if None
-# path to root of this repo, for constructing absolute paths to scripts
-PROJECT_DIR = (
-    Path(os.getenv("PROJECT_DIR"))
-    if "PROJECT_DIR" in os.environ
-    else Path("/home/snapdata/cmip6-utils/")
-)
-SCRATCH_DIR = (
-    Path(os.getenv("SCRATCH_DIR"))
-    if "SCRATCH_DIR" in os.environ
-    else Path("/beegfs/CMIP6/snapdata/")
-)
-conda_init_script = (
-    Path(os.getenv("CONDA_INIT"))
-    if "CONDA_INIT" in os.environ
-    else PROJECT_DIR.joinpath("indicators/conda_init.sh")
-)
-try:
-    slurm_email = Path(os.getenv("SLURM_EMAIL"))
-except TypeError:
-    slurm_email = None
-
-# this will probably not change between users
+slurm_email = "uaf-snap-sys-team@alaska.edu"
 cmip6_dir = Path("/beegfs/CMIP6/arctic-cmip6/")
 regrid_dir = Path("/beegfs/CMIP6/arctic-cmip6/regrid/")
-slurm_dir = SCRATCH_DIR.joinpath("slurm")
-
-# path to script for regridding
-indicators_script = PROJECT_DIR.joinpath("indicators/indicators.py")
-
 indicator_tmp_fp = "{indicator}_{model}_{scenario}_indicator.nc"
-
-sbatch_head_kwargs = {
-    # slurm info (doesn't need to be hardcoded, but OK for now?)
-    "partition": "t2small",
-    "ncpus": 24,
-    "conda_init_script": conda_init_script,
-    "slurm_email": slurm_email,
-}
