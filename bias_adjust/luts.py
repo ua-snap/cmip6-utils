7c60fd2a2e7adc9768e827c6cd151f5b00cb97e3
Kyle <kmredilla@alaska.edu>
2024-04-10 16:26:18 -0800

    add adjusted expected value ranges

--- bias_adjust/luts.py
+++ bias_adjust/luts.py
@@ -4,3 +4,15 @@ sim_ref_var_lu = {"tasmax": "t2mmax", "dtr": "dtr", "pr": "tp"}
 varid_adj_kind_lu = {"tasmax": "+", "dtr": "*", "pr": "*"}
 
 jitter_under_lu = {"pr": "0.01 mm d-1", "dtr": "1e-4 K"}
+
+# min / max possible ranges
+expected_value_ranges = {
+    # this value was determined in exploration
+    #  max temps for panarctic domain were ~45 C, min temps around -60 C
+    #  within day differences of 100 C are extremely unlikely
+    #  (units for data are kelvin but )
+    "dtr": {"minimum": 0, "maximum": 100}
+    # ~130 mm in 24 hrs is close to maximum for SE AK. 
+    # safe max is probably 150 mm, or in 1 kg m-2 s-1 
+    "pr": {"minimum": 0, "maximum": 1}
+}
