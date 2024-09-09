7bf8b11f5105e542a0ef37c55744e8c6dbc76fa7
Kyle <kmredilla@alaska.edu>
2024-09-09 13:33:54 -0800

    Combine qc script and notebook and simplify code

--- regridding/qc.py
+++ regridding/qc.py
@@ -15,28 +15,38 @@ from regrid import (
     generate_regrid_filepath,
     parse_output_filename_times_from_file,
     convert_units,
+    parse_cmip6_fp,
 )
 
 
-def get_source_fps_from_batch_files(regrid_batch_dir, var):
-    """For a given variable, use the batch files to get filenames of all source files that should have been regridded."""
-    # Return a list of all files inside *.txt that start with variable name
+def get_source_fps_from_batch_files(regrid_batch_dir):
+    """Get all of the source filepaths from the batch files in the regrid batch directory."""
     source_fps = []
     for fp in regrid_batch_dir.glob("*.txt"):
         with open(fp) as f:
-            # Return a list of all the files inside f with a basename that starts with args.variable.
-            source_fps.extend(
-                [
-                    Path(line.strip())
-                    for line in f
-                    if Path(line.strip()).name.startswith(var + "_")
-                ]
-            )
+            source_fps.extend([Path(line.strip()) for line in f])
 
     return source_fps
 
 
-def summarize_slurm_out_files(slurm_dir, error_file):
+def extract_params_from_src_filepath(src_fp):
+    """Assumes this is a source CMIP6 filepath."""
+    # .parts mapping for source: 7: model, 8: scenario, 10: frequency, 11: variable
+    params = parse_cmip6_fp(src_fp)
+
+    # the frequency could be something like Amon or Eday or whatever,
+    #  but we only use "day" or "mon" for the regridding
+    if "mon" in params["frequency"]:
+        params["frequency"] = "mon"
+    else:
+        # it should be daily if not monthly, just double check it
+        assert "day" in params["frequency"]
+        params["frequency"] = "day"
+
+    return params
+
+
+def summarize_slurm_out_files(slurm_dir):
     """Read all .out files in the slurm directory, and summarize overwrite/processing errors.
     Write processing errors to the qc error file.
     Return another list with all file paths that were not processed, to be ignored from subsequent QC steps.
@@ -62,151 +72,38 @@ def summarize_slurm_out_files(slurm_dir, error_file):
         print(
             f"Error: {len(error_lines)} source files were not regridded due to processing errors. There are no outputs to QC. Check qc/qc_error.txt for source file paths."
         )
-        with open(error_file, "a") as e:
-            e.write(
-                "The following source files had errors during the regridding process and do not have output files to QC:\n\n"
-            )
-            e.write(("\n".join(error_lines)))
-            e.write("\n")
+        _ = [print(line) for line in error_lines]
+
     return fps_to_ignore
 
 
-def compare_expected_to_existing_and_check_values(
-    regrid_dir,
-    regrid_batch_dir,
-    vars,
-    freqs,
-    models,
-    scenarios,
-    fps_to_ignore,
-    error_file,
-):
-    """Iterate through variables, comparing expected file paths to existing file paths.
-    If all expected files exist, check their values against source files.
-    Writes error messages to qc error file, and returns a list of fps with errors for printing a summary message.
-    """
-    # set up lists to collect error text
-    output_errors = []
-    ds_errors = []
-    value_errors = []
+def generate_expected_regrid_fps(src_fp, regrid_dir):
+    """Generate expected regrid filepaths from a source filepath."""
+    # build expected base file path from the source file path
+    expected_base_fp = generate_regrid_filepath(src_fp, regrid_dir)
+    base_timeframe = expected_base_fp.name.split("_")[-1].split(".nc")[0]
+    # get a list of yearly time range strings from the multi-year source filename
+    expected_filename_time_ranges = parse_output_filename_times_from_file(src_fp)
+    # replace the timeframe in the base file path with the yearly time ranges, and add to expected_fps list
+    expected_regrid_fps = []
+    for yearly_timeframe in expected_filename_time_ranges:
+        expected_fp = str(expected_base_fp).replace(base_timeframe, yearly_timeframe)
+        expected_regrid_fps.append(Path(expected_fp))
 
-    # create lists to populate with filepath/var tuples
-    regrid_var_tups = []
-    src_var_tups = []
+    return expected_regrid_fps
 
-    # iterate through all var/freq/model/scenario combos to populate the lists
-    for var in vars.split():
-        for freq in freqs.split():
-            for model in models.split():
-                for scenario in scenarios.split():
-                    # get existing regridded filepaths for the variable / frequency combos
-                    existing_fps = list(regrid_dir.glob(f"{model}/{scenario}/*{freq}/{var}/**/*.nc"))
 
-                    #existing_fps = [fp for fp in existing_fps]
-
-                    # populate list with tuples as args for the multiprocessing function
-                    for existing_fp in existing_fps:
-                        regrid_var_tups.append((existing_fp, var))
-
-                    # get source filepaths found in the batch files, ignoring the ones that had processing errors previously identified
-                    var_src_fps = get_source_fps_from_batch_files(regrid_batch_dir, var)
-                    for fp in fps_to_ignore:
-                        if fp in var_src_fps:
-                            var_src_fps.remove(fp)
-
-                    # populate list with tuples as args for the multiprocessing function
-                    for var_src_fp in var_src_fps:
-                        src_var_tups.append((var_src_fp, var))
-
-    # create dicts of min/max values for each regridded file and each source file
-    regrid_min_max = {}
-    src_min_max = {}
-    
-    # using multiprocessing, populate the dicts with min/max values for all regridded files and source files
-    with Pool(24) as p:
-        results = list(p.map(file_min_max, regrid_var_tups))
-    # populate min/max dict / store dataset errors
-    for result in results:
-        regrid_min_max[result["file"]] = {
-            "min": result["min"],
-            "max": result["max"],
-        }
-
-    with Pool(24) as p:
-        results = list(p.map(file_min_max, src_var_tups))
-    # populate min/max dict
-    for result in results:
-        src_min_max[result["file"]] = {
-            "min": result["min"],
-            "max": result["max"],
-        }
-
-    # create a list of expected regridded file paths from the source file paths
-    for src_fp in var_src_fps:
-        # build expected base file path from the source file path
-        expected_base_fp = generate_regrid_filepath(src_fp, regrid_dir)
-        base_timeframe = expected_base_fp.name.split("_")[-1].split(
-            ".nc"
-        )[0]
-        # get a list of yearly time range strings from the multi-year source filename
-        expected_filename_time_ranges = (
-            parse_output_filename_times_from_file(src_fp)
-        )
-        # replace the timeframe in the base file path with the yearly time ranges, and add to expected_fps list
-        expected_fps = []
-        for yearly_timeframe in expected_filename_time_ranges:
-            expected_fp = str(expected_base_fp).replace(
-                base_timeframe, yearly_timeframe
-            )
-            expected_fps.append(Path(expected_fp))
-
-        # search existing files for the expected files, and if not found add text to appropriate error list
-        # if all are found, run the final QC step to compare values
-        if all([fp in existing_fps for fp in expected_fps]):
-            # call min/max from src dict
-            src_min, src_max = (
-                src_min_max[str(src_fp)]["min"],
-                src_min_max[str(src_fp)]["max"],
-            )
-            # iterate thru expected filepaths
-            for regrid_fp in expected_fps:
-                # check if in keys, if not then the file did not open in file_min_max()
-                if str(regrid_fp) in regrid_min_max.keys():
-                    # compare values
-                    regrid_min, regrid_max = (
-                        regrid_min_max[str(regrid_fp)]["min"],
-                        regrid_min_max[str(regrid_fp)]["max"],
+def generate_regrid_fps_from_params(models, scenarios, vars, freqs, regrid_dir):
+    """Expecting lists of models, scenarios, vars, and freqs (as supplied to the notebook)."""
+    regrid_fps = []
+    for model in models.split():
+        for scenario in scenarios.split():
+            for var in vars.split():
+                for freq in freqs.split():
+                    regrid_fps += list(
+                        regrid_dir.glob(f"{model}/{scenario}/*{freq}/{var}/*.nc")
                     )
-                    if (src_max >= regrid_min >= src_min) and (
-                        src_max >= regrid_max >= src_min
-                    ):
-                        pass
-                    else:
-                        value_errors.append(str(regrid_fp))
-                else:
-                    ds_errors.append(str(regrid_fp))
-        else:
-            output_errors.append(str(src_fp))
-
-    # write all errors to qc_error.txt
-    with open(error_file, "a") as e:
-        if output_errors != []:
-            e.write(
-                "Could not find all expected regridded output files for the following source files:\n\n"
-            )
-            e.write(("\n".join(map(str, output_errors))))
-            e.write("\n\n")
-        if ds_errors != []:
-            e.write("Could not open datasets for the following regridded files:\n\n")
-            e.write(("\n".join(map(str, ds_errors))))
-            e.write("\n\n")
-        if value_errors != []:
-            e.write(
-                "Values outside source range for the following regridded files:\n\n"
-            )
-            e.write(("\n".join(map(str, value_errors))))
-            e.write("\n\n")
-    return output_errors, ds_errors, value_errors
+    return regrid_fps
 
 
 def make_qc_file(output_directory):
@@ -219,18 +116,17 @@ def make_qc_file(output_directory):
     return error_file
 
 
-def file_min_max(args):
+def file_min_max(fp):
     """Get file min and max values within the Arctic latitudes."""
-    file, var = args
-    print(file)
-    print(str(datetime.now()))
     try:
         try:
             # using the h5netcdf engine because it seems faster and might help prevent pool hanging
-            src_ds = xr.open_dataset(file, engine="h5netcdf")
+            src_ds = xr.open_dataset(fp, engine="h5netcdf")
         except:
             # this seems to have only failed due to some files (KACE model) being written in netCDF3 format
-            src_ds = xr.open_dataset(file)
+            src_ds = xr.open_dataset(fp)
+
+        var_id = get_var_id(src_ds)
 
         # handle regridded data being flipped
         if src_ds.lat[0] > src_ds.lat[-1]:
@@ -242,101 +138,100 @@ def file_min_max(args):
 
         src_ds_slice = convert_units(src_ds_slice)
 
-        src_min, src_max = float(src_ds_slice[var].min()), float(
-            src_ds_slice[var].max()
+        src_min, src_max = float(src_ds_slice[var_id].min()), float(
+            src_ds_slice[var_id].max()
         )
-        return {"file": str(file), "min": src_min, "max": src_max}
+        return {"file": str(fp), "min": src_min, "max": src_max}
     except:
         return {"file": None, "min": None, "max": None}
 
 
-def parse_args():
-    """Parse arguments"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "--output_directory",
-        type=str,
-        help="Path to directory where all regridded data was written",
-        required=True,
-    )
-    parser.add_argument(
-        "--vars",
-        type=str,
-        help="list of variables",
-        required=True,
-    )
-    parser.add_argument(
-        "--freqs",
-        type=str,
-        help="list of frequencies used in generating batch files",
-        required=True,
-    )
-    parser.add_argument(
-        "--models",
-        type=str,
-        help="list of models used in generating batch files",
-        required=True,
-    )
-    parser.add_argument(
-        "--scenarios",
-        type=str,
-        help="list of scenarios used in generating batch files",
-        required=True,
-    )
-    args = parser.parse_args()
-    return (
-        Path(args.output_directory),
-        args.vars,
-        args.freqs,
-        args.models,
-        args.scenarios,
+def compare_expected_to_existing_and_check_values(
+    regrid_dir,
+    regrid_batch_dir,
+    vars,
+    freqs,
+    models,
+    scenarios,
+    fps_to_ignore,
+):
+    """Iterate through model / scenario/ frequency/ variable combos, comparing data from expected file paths to existing file paths.
+    If all expected files exist, check their values against source files.
+    Writes error messages to qc error file, and returns a list of fps with errors for printing a summary message.
+    """
+    # set up lists to collect error text
+    source_files_missing_regrids = []
+    ds_errors = []
+    value_errors = []
+
+    src_fps = get_source_fps_from_batch_files(regrid_batch_dir)
+
+    fps_to_ignore = summarize_slurm_out_files(slurm_dir)
+    for fp in fps_to_ignore:
+        if fp in src_fps:
+            src_fps.remove(fp)
+
+    existing_regrid_fps = generate_regrid_fps_from_params(
+        models, scenarios, vars, freqs, regrid_dir
     )
 
+    # create dicts of min/max values for each regridded file and each source file
+    regrid_min_max = {}
+    src_min_max = {}
 
-if __name__ == "__main__":
+    # using multiprocessing, populate the dicts with min/max values for all regridded files and source files
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as pool:
+        results = list(pool.map(file_min_max, existing_regrid_fps))
 
-    output_directory, vars, freqs, models, scenarios = parse_args()
-    regrid_dir = output_directory.joinpath("regrid")
-    regrid_batch_dir = output_directory.joinpath("regrid_batch")
-    slurm_dir = output_directory.joinpath("slurm", "regrid")
-    error_file = make_qc_file(output_directory)
+    # populate min/max dict / store dataset errors
+    for result in results:
+        regrid_min_max[result["file"]] = {
+            "min": result["min"],
+            "max": result["max"],
+        }
 
-    print("QC process started...")
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as pool:
+        results = list(pool.map(file_min_max, src_fps))
 
-    # this might help multiprocessing Pool better
-    set_start_method("spawn")
+    # populate min/max dict
+    for result in results:
+        src_min_max[result["file"]] = {
+            "min": result["min"],
+            "max": result["max"],
+        }
 
-    # check slurm files
-    fps_to_ignore = summarize_slurm_out_files(slurm_dir, error_file)
+    # create a list of expected regridded file paths from the source file paths
+    for src_fp in src_fps:
+        expected_regrid_fps = generate_expected_regrid_fps(src_fp, regrid_dir)
 
-    # check if all expected files exist, check for dataset opening & reasonable values
-    (
-        output_errors,
-        ds_errors,
-        value_errors,
-    ) = compare_expected_to_existing_and_check_values(
-        regrid_dir,
-        regrid_batch_dir,
-        vars,
-        freqs,
-        models,
-        scenarios,
-        fps_to_ignore,
-        error_file,
-    )
+        # search existing files for the expected files, and if not found add text to appropriate error list
+        # if all are found, run the final QC step to compare values
 
-    # print summary messages
-    error_count = len(output_errors) + len(ds_errors) + len(value_errors)
-    print(f"QC process complete: {error_count} errors found.")
-    if len(output_errors) > 0:
-        print(
-            f"Errors found when looking for expected output files. {len(output_errors)} files are missing expected outputs. See {str(error_file)} for error log."
+        if not all([fp in existing_regrid_fps for fp in expected_regrid_fps]):
+            source_files_missing_regrids.append(str(src_fp))
+
+        # only want to run the summaries on source files that have all expected regrids
+        # call min/max from src dict
+        src_min, src_max = (
+            src_min_max[str(src_fp)]["min"],
+            src_min_max[str(src_fp)]["max"],
         )
-    if len(ds_errors) > 0:
-        print(
-            f"Errors in opening some datasets. {len(ds_errors)} files could not be opened. See {str(error_file)} for error log."
-        )
-    if len(value_errors) > 0:
-        print(
-            f"Errors in dataset values. {len(value_errors)} files have regridded values outside of source file range. See {str(error_file)} for error log."
-        )
+        # iterate thru expected filepaths
+        for regrid_fp in expected_regrid_fps:
+            # check if in keys, if not then the file did not open in file_min_max()
+            if str(regrid_fp) in regrid_min_max.keys():
+                # compare values
+                regrid_min, regrid_max = (
+                    regrid_min_max[str(regrid_fp)]["min"],
+                    regrid_min_max[str(regrid_fp)]["max"],
+                )
+                if (src_max >= regrid_min >= src_min) and (
+                    src_max >= regrid_max >= src_min
+                ):
+                    pass
+                else:
+                    value_errors.append(str(regrid_fp))
+            else:
+                ds_errors.append(str(regrid_fp))
+
+    return ds_errors, value_errors
