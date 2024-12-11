22f46753041314ef325b54278acc6e93ff05a056
kyleredilla <kmredilla@alaska.edu>
2024-12-11 12:01:07 -0900

    check for latlon dims before fixing

--- regridding/regrid.py
+++ regridding/regrid.py
@@ -468,16 +468,16 @@ def rasdafy(ds):
     # make sure the time axis is unlimited (this means it is a "record dimension" in netCDF parlance)
     ds.encoding["unlimited_dims"] = ["time"]
 
-    # make sure the latitude axis is in decreasing order
-    if ds.lat.values[0] < ds.lat.values[-1]:
-        ds = ds.sel(lat=slice(None, None, -1))
+    # make sure the latitude dim is in decreasing order
+    if "lat" in ds.dims:
+        if ds.lat.values[0] < ds.lat.values[-1]:
+            ds = ds.sel(lat=slice(None, None, -1))
+        # make sure the dims are ordered (time, lon, lat)
+        ds = ds.transpose("time", "lon", "lat")
 
     # drop bnds dimension if it exists
     for bnd_dim in ["bnds", "nbnd", "lat_bnds", "lon_bnds", "time_bnds"]:
         ds = ds.drop_dims(bnd_dim, errors="ignore")
-
-    # make sure the axes are ordered (time, lon, lat)
-    ds = ds.transpose("time", "lon", "lat")
 
     ds = convert_units(ds)
 
