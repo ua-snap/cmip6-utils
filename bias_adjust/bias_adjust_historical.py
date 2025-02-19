58adcd181bbb011692425574e4eb427d0f228fef
Kyle <kmredilla@alaska.edu>
2025-02-19 14:06:53 -0900

    save trained object for historical adjustment plus dask tweaks

--- bias_adjust/bias_adjust_historical.py
+++ bias_adjust/bias_adjust_historical.py
@@ -1,7 +1,7 @@
 """Script to adjust historical data for a given model. Will simply omit the ScenarioMIP years and shift the overlapping historical years back. 
 
 Usage:
-    python bias_adjust.py --var_id pr --model GFDL-ESM4 --input_dir /import/beegfs/CMIP6/kmredilla/cmip6_regridding/regrid --reference_dir /beegfs/CMIP6/arctic-cmip6/era5/daily_regrid --adj_dir /import/beegfs/CMIP6/kmredilla/bias_adjust/netcdf
+    python bias_adjust_historical.py --var_id tasmax --model GFDL-ESM4 --input_dir /import/beegfs/CMIP6/kmredilla/cmip6_4km_3338/regrid --reference_dir /beegfs/CMIP6/kmredilla/downscaling/era5_3338 --adj_dir /beegfs/CMIP6/kmredilla/cmip6_4km_3338_adjusted/netcdf
 """
 
 import argparse
@@ -78,15 +78,12 @@ if __name__ == "__main__":
     # get reference files
     ref_var_id = sim_ref_var_lu[var_id]
     # We will call the "hist" years those which are the same as the reference years
-    #  which we will be using for training. We will actually adjust further back than
-    #  this, back to 1950.
-    ref_start_year = 1981
+    #  which we will be using for training.
+    ref_start_year = 1984
     ref_end_year = 2014
     ref_years = list(range(ref_start_year, ref_end_year + 1))
     ref_fps = [
-        reference_dir.joinpath(ref_var_id).joinpath(
-            ref_tmp_fn.format(ref_var_id=ref_var_id, year=year)
-        )
+        reference_dir.joinpath(ref_tmp_fn.format(ref_var_id=ref_var_id, year=year))
         for year in ref_years
     ]
 
@@ -97,7 +94,7 @@ if __name__ == "__main__":
     ]
 
     # get modeled historical files that we will be adjusting
-    hist_start_year = 1950
+    hist_start_year = 1984
     hist_end_year = 2014
     hist_years = list(range(hist_start_year, hist_end_year + 1))
     hist_fps = [
@@ -108,9 +105,14 @@ if __name__ == "__main__":
     kind = varid_adj_kind_lu[var_id]
 
     # suggestion from dask for ignoring large chunk warnings
-    with dask.config.set(**{"array.slicing.split_large_chunks": False}):
+    with dask.config.set(
+        **{
+            "array.slicing.split_large_chunks": False,
+            "temporary_directory": "/beegfs/CMIP6/kmredilla/tmp",
+        }
+    ):
         # messed around with the dask config a lot. This works but generates lots of GC warnings. Best I found though.
-        with Client(n_workers=20, threads_per_worker=1) as client:
+        with Client(n_workers=10, threads_per_worker=1) as client:
             # open connection to data
             hist_ref_ds = xr.open_mfdataset(hist_ref_fps)
             # convert calendar to noleap to match CMIP6
@@ -124,8 +126,9 @@ if __name__ == "__main__":
             # need to re-chunk the data - cannot have multiple chunks along the adjustment dimension (time)
             ref = ref_ds[ref_var_id]
             hist = hist_ref_ds[var_id]
-            ref.data = ref.data.rechunk({0: -1, 1: 30, 2: 30})
-            hist.data = hist.data.rechunk({0: -1, 1: 30, 2: 30})
+
+            ref.data = ref.data.rechunk({0: -1, 1: 15, 2: 15})
+            hist.data = hist.data.rechunk({0: -1, 1: 15, 2: 15})
 
             if var_id == "pr":
                 # need to set the correct compatible precipitation units for ERA5 if precip
@@ -155,29 +158,22 @@ if __name__ == "__main__":
                 train_kwargs.update(adapt_freq_thresh="1 mm d-1")
 
             # dqm = sdba.DetrendedQuantileMapping.train(**train_kwargs)
-            dqm = sdba.QuantileDeltaMapping.train(**train_kwargs)
+            qdm = sdba.QuantileDeltaMapping.train(**train_kwargs)
+
+            trained_dir = adj_dir.parent.joinpath("trained")
+            trained_dir.mkdir(exist_ok=True)
+            trained_fp = trained_dir.joinpath(f"qdm_{var_id}_{model}_{scenario}.nc")
+            print(f"Writing QDM object to {trained_fp}")
+            qdm.ds.to_netcdf(trained_fp)
 
             # Create the detrending object
             det = LoessDetrend(
                 group="time.dayofyear", d=0, niter=1, f=0.2, weights="tricube"
             )
 
-            # create a dataset containing all historical data to be adjusted
-            # calling this sim to denote that these are all the simulated data we want to adjust
-            # need to rechunk this one too, same reason as for training data
-            sim_ds = xr.open_mfdataset(hist_fps)
-            sim_ds = drop_height(sim_ds)
-            sim = sim_ds[var_id]
-            sim.data = sim.data.rechunk({0: -1, 1: 30, 2: 30})
-
-            # free up memory? dunno if this helps
-            del ref
-            del hist
-
             scen = (
-                dqm.adjust(sim, extrapolation="constant", interp="nearest", detrend=det)
-                # in testing, adjusted outputs are oriented lat, lon, time for some reason
-                .transpose("time", "lat", "lon")
+                # dqm.adjust(sim, extrapolation="constant", interp="nearest", detrend=det)
+                qdm.adjust(hist, extrapolation="constant", interp="nearest")
             )
             # doing the computation here seems to help with performance
             scen.load()
