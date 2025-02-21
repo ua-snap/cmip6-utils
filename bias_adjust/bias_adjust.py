6bbba0d91dca64b156f2cbdd12b596f478ac6cd3
Kyle <kmredilla@alaska.edu>
2025-02-20 15:50:10 -0900

    load trained dataset from adjustment script

--- bias_adjust/bias_adjust.py
+++ bias_adjust/bias_adjust.py
@@ -1,11 +1,12 @@
-"""
+"""Script for bias adjusting a given model and scenario. Uses a pre-trained quantile mapping adjustment object.
 
 Usage:
-    python bias_adjust.py --var_id pr --model GFDL-ESM4 --scenario ssp585 --input_dir /import/beegfs/CMIP6/kmredilla/cmip6_regridding/regrid --reference_dir /beegfs/CMIP6/arctic-cmip6/era5/daily_regrid --adj_dir /import/beegfs/CMIP6/kmredilla/bias_adjust/netcdf
+    python bias_adjust.py --train_fp /import/beegfs/CMIP6/kmredilla/bias_adjust/trained/qdm_pr_GFDL-ESM4_ssp585.nc --var_id pr --model GFDL-ESM4 --scenario ssp585 --input_dir /import/beegfs/CMIP6/kmredilla/cmip6_regridding/regrid --adj_dir /import/beegfs/CMIP6/kmredilla/bias_adjust/adjusted
 """
 
 import argparse
 import datetime
+import re
 import multiprocessing as mp
 from itertools import product
 from pathlib import Path
@@ -15,8 +16,35 @@ import dask
 from dask.distributed import Client
 from xclim import sdba
 from xclim.sdba.detrending import LoessDetrend
-from config import ref_tmp_fn, cmip6_tmp_fn
+from config import ref_tmp_fn, cmip6_tmp_fn, train_tmp_fn
 from luts import sim_ref_var_lu, varid_adj_kind_lu, jitter_under_lu
+
+
+def extract_values_from_format(format_str, formatted_str, keys):
+    """Extract values from a formatted string using the original format string"""
+    # Create a regex pattern from the format string
+    pattern = re.sub(r"{(\w+)}", r"(?P<\1>.+)", format_str)
+    match = re.match(pattern, formatted_str)
+    if match:
+        return {key: match.group(key) for key in keys}
+    else:
+        raise ValueError(f"Keys {keys} not found in the formatted string")
+
+
+def validate_train_fp(train_fp, model, scenario, var_id):
+    """Validate the train_fp by checking the attributes in the filename against the script parameters"""
+    train_fp_attrs = extract_values_from_format(
+        train_tmp_fn, train_fp.name, ["method", "var_id", "model", "scenario"]
+    )
+    if (
+        train_fp_attrs["model"] != model
+        or train_fp_attrs["scenario"] != scenario
+        or train_fp_attrs["var_id"] != var_id
+    ):
+        raise ValueError(
+            f"Model, scenario, or var_id in train_fp does not match the input values"
+        )
+    return train_fp
 
 
 def generate_adjusted_filepaths(adj_dir, var_ids, models, scenarios, years):
@@ -69,6 +97,16 @@ def generate_cmip6_fp(input_dir, model, scenario, var_id, year):
     )
 
     return src_fp
+
+
+def get_sim_fps(input_dir, model, scenario, var_id, start_year, end_year):
+    """Get the filepaths to the simulated data to be adjusted"""
+    sim_years = list(range(start_year, end_year + 1))
+    sim_fps = [
+        generate_cmip6_fp(input_dir, model, scenario, var_id, year)
+        for year in sim_years
+    ]
+    return sim_fps
 
 
 def add_global_attrs(ds, src_fp):
@@ -125,6 +163,12 @@ def parse_args():
     """Parse some arguments"""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
+        "--train_fp",
+        type=str,
+        help="Path to trained quantile mapping adjustment netcdf file",
+        required=True,
+    )
+    parser.add_argument(
         "--var_id",
         type=str,
         help="Variable ID to adjust",
@@ -143,9 +187,21 @@ def parse_args():
         required=True,
     )
     parser.add_argument(
-        "--input_dir",
+        "--sim_dir",
         type=str,
         help="Path to directory of simulated data files to be adjusted, with filepath structure <model>/<scenario>/day/<variable ID>/<files>",
+    )
+    parser.add_argument(
+        "--sim_start_year",
+        type=str,
+        help="Starting year of simulated data to be adjusted",
+        required=True,
+    )
+    parser.add_argument(
+        "--sim_end_year",
+        type=str,
+        help="Ending year of simulated data to be adjusted",
+        required=True,
     )
     parser.add_argument(
         "--reference_dir",
@@ -160,10 +216,13 @@ def parse_args():
     args = parser.parse_args()
 
     return (
+        Path(args.train_fp),
         args.var_id,
         args.model,
         args.scenario,
-        Path(args.input_dir),
+        Path(args.sim_dir),
+        sim_start_year,
+        sim_end_year,
         Path(args.reference_dir),
         Path(args.adj_dir),
     )
@@ -171,44 +230,22 @@ def parse_args():
 
 if __name__ == "__main__":
     (
+        train_fp,
         var_id,
         model,
         scenario,
-        input_dir,
+        sim_dir,
+        sim_start_year,
+        sim_end_year,
         reference_dir,
         adj_dir,
     ) = parse_args()
 
-    # get reference files
-    ref_var_id = sim_ref_var_lu[var_id]
-    ref_start_year = 1993
-    ref_end_year = 2022
-    ref_years = list(range(ref_start_year, ref_end_year + 1))
-    ref_fps = [
-        reference_dir.joinpath(ref_var_id).joinpath(
-            ref_tmp_fn.format(ref_var_id=ref_var_id, year=year)
-        )
-        for year in ref_years
-    ]
+    train_fp = validate_train_fp(train_fp, model, scenario, var_id)
 
-    # get modeled historical files
-    hist_start_year = 1993
-    hist_end_year = 2014
-    hist_years = list(range(hist_start_year, hist_end_year + 1))
-    hist_fps = [
-        generate_cmip6_fp(input_dir, model, "historical", var_id, year)
-        for year in hist_years
-    ]
-
-    # get modeled projected files for the remainder of reference period not available in modeled historical
-    # these are the years we will be including from ScenarioMIP projections to match the reference period
-    sim_ref_start_year = 2015
-    sim_ref_end_year = 2022
-    sim_ref_years = list(range(sim_ref_start_year, sim_ref_end_year + 1))
-    sim_ref_fps = [
-        generate_cmip6_fp(input_dir, model, scenario, var_id, year)
-        for year in sim_ref_years
-    ]
+    sim_fps = get_sim_fps(
+        input_dir, model, scenario, var_id, sim_start_year, sim_end_year
+    )
 
     # get all remaining projected files
     sim_start_year = 2023
@@ -216,8 +253,7 @@ if __name__ == "__main__":
     sim_end_year = 2100
     sim_years = list(range(sim_start_year, sim_end_year + 1))
     sim_fps = [
-        generate_cmip6_fp(input_dir, model, scenario, var_id, year)
-        for year in sim_years
+        generate_cmip6_fp(sim_dir, model, scenario, var_id, year) for year in sim_years
     ]
 
     kind = varid_adj_kind_lu[var_id]
@@ -226,54 +262,9 @@ if __name__ == "__main__":
     with dask.config.set(**{"array.slicing.split_large_chunks": False}):
         # messed around with the dask config a lot. This works but generates lots of GC warnings. Best I found though.
         with Client(n_workers=20, threads_per_worker=1) as client:
-            # open connection to data
-            hist_ds = xr.open_mfdataset(hist_fps + sim_ref_fps)
-            # convert calendar to noleap to match CMIP6
-            ref_ds = xr.open_mfdataset(ref_fps).convert_calendar("noleap")
-
-            # height variable is causing some issues in some cases and we don't need
-            #  it for the bias-adjusted stuff, just drop it.
-            hist_ds = drop_height(hist_ds)
-            ref_ds = drop_height(ref_ds)
-
-            # need to select ERA5 or ERA5T if present
-            if "expver" in ref_ds.variables:
-                ref_ds = ref_ds.sel(expver=1).drop_vars("expver")
-
-            # need to re-chunk the data - cannot have multiple chunks along the adjustment dimension (time)
-            ref = ref_ds[ref_var_id]
-            hist = hist_ds[var_id]
-            ref.data = ref.data.rechunk({0: -1, 1: 30, 2: 30})
-            hist.data = hist.data.rechunk({0: -1, 1: 30, 2: 30})
-
-            if var_id == "pr":
-                # need to set the correct compatible precipitation units for ERA5 if precip
-                ref.attrs["units"] = "m d-1"
-
-            # ensure data does not have zeros, depending on variable
-            if var_id in jitter_under_lu.keys():
-                jitter_under_thresh = jitter_under_lu[var_id]
-                ref = sdba.processing.jitter_under_thresh(
-                    ref, thresh=jitter_under_thresh
-                )
-                hist = sdba.processing.jitter_under_thresh(
-                    hist, thresh=jitter_under_thresh
-                )
-            print("jitter done")
-
-            train_kwargs = dict(
-                ref=ref,
-                hist=hist,
-                nquantiles=50,
-                group="time.dayofyear",
-                window=31,
-                kind=kind,
-            )
-            if var_id == "pr":
-                # do the adapt frequency thingy for precipitation data
-                train_kwargs.update(adapt_freq_thresh="1 mm d-1")
-
-            dqm = sdba.DetrendedQuantileMapping.train(**train_kwargs)
+            # open connection to trained QM dataset
+            train_ds = xr.open_dataset(train_fp)
+            dqm = sdba.QuantileDeltaMapping.from_dataset(train_ds)
 
             # Create the detrending object
             det = LoessDetrend(
@@ -283,26 +274,20 @@ if __name__ == "__main__":
             # create a dataset containing all projected data to be adjusted
             # not adjusting historical, no need for now
             # need to rechunk this one too, same reason as for training data
-            proj_ds = xr.open_mfdataset(sim_ref_fps + sim_fps)
-            proj_ds = drop_height(proj_ds)
-            sim = proj_ds[var_id]
-            sim.data = sim.data.rechunk({0: -1, 1: 30, 2: 30})
-
-            # free up memory? dunno if this helps
-            del ref
-            del hist
+            sim_ds = xr.open_mfdataset(sim_fps)
+            sim_ds = drop_height(sim_ds)
+            sim = sim_ds[var_id]
+            sim.data = sim.data.rechunk({0: -1, 1: 15, 2: 15})
 
             scen = (
-                dqm.adjust(sim, extrapolation="constant", interp="nearest", detrend=det)
-                # in testing, adjusted outputs are oriented lat, lon, time for some reason
-                .transpose("time", "lat", "lon")
+                # dqm.adjust(sim, extrapolation="constant", interp="nearest", detrend=det)
+                dqm.adjust(sim, extrapolation="constant", interp="nearest")
             )
             # doing the computation here seems to help with performance
             scen.load()
 
-    adj_years = sim_ref_years + sim_years
     # now write the adjusted data to disk by year
-    for year in adj_years:
+    for year in sim_years:
         adj_fp = generate_adjusted_filepaths(
             adj_dir, [var_id], [model], [scenario], [year]
         )[0]
@@ -314,9 +299,8 @@ if __name__ == "__main__":
             ["time", "lat", "lon", var_id]
         ]
         # get the source CMIP6 data file used for the attributes
-        src_fp = generate_cmip6_fp(input_dir, model, scenario, var_id, year)
+        src_fp = generate_cmip6_fp(sim_dir, model, scenario, var_id, year)
         out_ds = add_global_attrs(out_ds, src_fp)
         out_ds.to_netcdf(adj_fp)
 
         print(year, "done", end=", ")
-print()
