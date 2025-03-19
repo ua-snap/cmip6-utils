aeccd5276b7037900e492f763dfd0d1cf8c79dfd
Kyle <kmredilla@alaska.edu>
2025-03-19 13:52:59 -0800

    functions for loading data, indicators, and plotting

--- bias_adjust/eda_book/baeda.py
+++ bias_adjust/eda_book/baeda.py
@@ -3,9 +3,12 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 from xclim import units, sdba, indices
+from xclim.core.calendar import percentile_doy
 import numpy as np
 import xarray as xr
-
+from pyproj import Proj
+import pandas as pd
+import seaborn as sns
 
 # some constants for the paths to the data and such
 cmip6_tmp_fn = "{var_id}_day_{model}_{scenario}_regrid_{year}0101-{year}1231.nc"
@@ -14,12 +17,126 @@ cmip6_dir = Path("/beegfs/CMIP6/kmredilla/cmip6_regridding/regrid")
 
 # lookups for functions and indices
 doy_func_lu = {"min": np.min, "max": np.max, "mean": np.mean}
+
+
+def wsdi(tasmax):
+    """Warm spell duration index."""
+    # use the ERA5 as baseline reference for WSDI percentiles
+    hist_da = tasmax.sel(Method="ERA5")
+    tasmax_per = percentile_doy(hist_da, per=90).sel(percentiles=90)
+    da = indices.warm_spell_duration_index(tasmax, tasmax_per, window=6).drop(
+        "percentiles"
+    )
+    da.name = "wsdi"
+    da.attrs["long_name"] = "Warm spell duration index"
+    return da
+
+
+def ice_days(
+    tasmax,
+):
+    """Icing days."""
+    da = indices.tx_days_below(tasmax, thresh="0.0 degC")
+    da.name = "id"
+    da.attrs["long_name"] = "Icing days"
+    return da
+
+
+def summer_days(tasmax):
+    """Summer days."""
+    # threshold for summer days is 20.0 degC because Alaska
+    da = indices.tx_days_above(tasmax, thresh="20.0 degC")
+    da.name = "su"
+    da.attrs["long_name"] = "Summer days"
+    return da
+
+
+def tx90p(tasmax):
+    hist_da = tasmax.sel(Method="ERA5")
+    tasmax_per = percentile_doy(hist_da, per=90).sel(percentiles=90)
+    da = indices.tx90p(tasmax, tasmax_per)
+    da.name = "tx90p"
+    da.attrs["long_name"] = "Days above 90th percentile"
+    return da
+
+
+def rx1day(pr):
+    """Maximum 1-day precipitation."""
+    da = indices.max_1day_precipitation_amount(pr)
+    da.name = "rx1day"
+    da.attrs["long_name"] = "Maximum 1-day precipitation"
+    return da
+
+
+def rx5day(pr):
+    """Maximum 5-day precipitation."""
+    da = indices.max_n_day_precipitation_amount(pr, window=5)
+    da.name = "rx5day"
+    da.attrs["long_name"] = "Maximum 5-day precipitation"
+    return da
+
+
+def cdd(pr):
+    """Consecutive dry days."""
+    da = indices.maximum_consecutive_dry_days(pr)
+    da.name = "cdd"
+    da.attrs["long_name"] = "Maximum consecutive dry days"
+    return da
+
+
+def cwd(pr):
+    """Consecutive wet days."""
+    da = indices.maximum_consecutive_wet_days(pr)
+    da.name = "cwd"
+    da.attrs["long_name"] = "Maximum consecutive wet days"
+    return da
+
+
 indices_lu = {
-    "mcdd": indices.maximum_consecutive_dry_days,
-    "dstl": indices.dry_spell_total_length,
-    "mcwd": indices.maximum_consecutive_wet_days,
-    "wstl": indices.wet_spell_total_length,
+    "pr": {
+        "rx1day": rx1day,
+        "rx5day": rx5day,
+        "cdd": cdd,
+        "cwd": cwd,
+    },
+    #     "dstl": indices.dry_spell_total_length,
+    #     "mcwd": indices.maximum_consecutive_wet_days,
+    #     "wstl": indices.wet_spell_total_length,
+    # },
+    "tasmax": {
+        "su": summer_days,
+        "id": ice_days,
+        "wsdi": wsdi,
+        "tx90p": tx90p,
+    },
 }
+
+# these are some different detrending methods for the DQM
+detrend_configs = {
+    "Loess DQM": sdba.detrending.LoessDetrend(
+        group="time.dayofyear", d=0, niter=1, f=0.2, weights="tricube"
+    ),
+    "Mean DQM": sdba.detrending.MeanDetrend(group="time.dayofyear"),
+    "Poly1 DQM": sdba.detrending.PolyDetrend(group="time.dayofyear", degree=1),
+    "Poly3 DQM": sdba.detrending.PolyDetrend(group="time.dayofyear", degree=3),
+}
+
+# This is a dict of locations with lat/lon coords for rapid testing
+coords = {
+    "Fairbanks": {"lat": 64.8401, "lon": -147.72},
+    "Anchorage": {"lat": 61.2176, "lon": -149.8997},
+    "Nome": {"lat": 64.5006, "lon": -165.4086},
+    "Yakutat": {"lat": 59.5453, "lon": -139.7268},
+    "Utqiagvik": {"lat": 71.2906, "lon": -156.7886},
+}
+
+
+era5_var_id_lu = {"t2max": "tasmax", "pr": "pr"}
+jitter_under_thresh_lu = {"pr": "0.01 mm d-1", "dtr": "1e-4 K"}
+adapt_freq_thresh_lu = {"pr": "1 mm d-1"}
+units_lu = {"pr": "mm d-1", "tasmax": "degC", "dtr": "degC"}
+varid_adj_kind_lu = {"tasmax": "+", "tasmin": "+", "dtr": "*", "pr": "*"}
+
 
 # we will be getting filepaths in a similar way many times. Make some functions to make this easier
 def get_cmip6_fps(cmip6_dir, model, scenario, var_id, start_year, end_year):
@@ -191,3 +308,274 @@ def get_hist(model_dir, var_id, chunks={"lat_chunk": 60, "lon_chunk": 60}):
     hist = get_rechunked_da(hist_ds, var_id, **chunks)
 
     return hist
+
+
+### everything below here is from the more recent explore_qdm.ipynb efforts for the bias adjustment piece of statistical downscaling
+
+
+def get_projected_coords(zarr_store, coords):
+    """Get the projected coordinates for the given coordinates."""
+    with xr.open_zarr(zarr_store) as ds:
+
+        # Define the WKT projection for the ERA5 data
+        wkt_projection = ds.spatial_ref.attrs["crs_wkt"]
+
+        proj = Proj(wkt_projection)
+
+        # Convert lat, lon to the projection system
+        projected_coords = {}
+        for point, latlon in coords.items():
+            x, y = proj(latlon["lon"], latlon["lat"])
+            projected_coords[point] = {"x": x, "y": y}
+
+    return projected_coords
+
+
+def extract_time_series(ds, var_id, projected_coords):
+    """Extracts the time series of the given coordinates from the dataset. returns a dictionary of time series."""
+    time_series = {}
+    for location, coord in projected_coords.items():
+        x, y = projected_coords[location]["x"], projected_coords[location]["y"]
+        time_series[location] = (
+            ds[var_id].sel(x=x, y=y, method="nearest").drop_vars(["x", "y"])
+        )
+
+    # Combine the time series into a single dataset
+    combined_time_series = xr.concat(time_series.values(), dim="location")
+    combined_time_series["location"] = list(time_series.keys())
+
+    return combined_time_series
+
+
+def adjust_and_combine_dqm_qdm(dqm_train, qdm_train, sim):
+    """Run adjustments for all iterations of detrending, plus the qdm method, and combine into a single dataset.
+    Triggers computation of returned dataarray."""
+    adj_das = []
+    for det_name, det in detrend_configs.items():
+        scen = dqm_train.adjust(
+            sim,
+            extrapolation="constant",
+            interp="nearest",
+            detrend=det,
+        )
+        adj_das.append(scen)
+
+    # run the QDM adjustment
+    qdm_da = qdm_train.adjust(
+        sim,
+        extrapolation="constant",
+        interp="nearest",
+    )
+    adj_das.append(qdm_da)
+
+    # combine the DQM and QDM data
+    adj_da = xr.concat(adj_das, dim="Method").rename(sim.name)
+    adj_da["Method"] = list(detrend_configs.keys()) + ["QDM"]
+
+    return adj_da.load()
+
+
+def drop_non_coord_vars(ds):
+    """Function to drop all coordinates from xarray dataset which are not coordinate variables, i.e. which are not solely indexed by a dimension of the same name
+
+    Args:
+        ds (xarray.Dataset): dataset to drop non-coordinate-variables from
+
+    Returns:
+        ds (xarray.Dataset): dataset with only dimension coordinates
+    """
+    coords_to_drop = [coord for coord in ds.coords if ds[coord].dims != (coord,)]
+    # some datasets have a variables such as spatial_ref that are indexed by time and should be dropped.
+    vars_to_drop = [var for var in ds.data_vars if len(ds[var].dims) < 3]
+    ds = ds.drop_vars(coords_to_drop + vars_to_drop)
+
+    return ds
+
+
+def open_era5_dataset(era5_stores):
+    """Open the ERA5 dataset from the given zarr stores."""
+    # open and convert calendar to noleap
+    era5_ds = xr.merge(
+        [xr.open_zarr(store) for store in era5_stores.values()]
+    ).convert_calendar("noleap")
+
+    era5_ds = era5_ds.assign_coords(time=era5_ds.time.dt.floor("D"))
+
+    # set the correct compatible precipitation units for ERA5 if precip
+    for era5_var_id in era5_stores.keys():
+        var_id = era5_var_id_lu[era5_var_id]
+        # first rename the variable to the shared CMIP6 name
+        era5_ds = era5_ds.rename({era5_var_id: var_id})
+        # Make units proper if needed
+        if era5_ds[var_id].attrs["units"] == "mm":
+            era5_ds[var_id].attrs["units"] = "mm d-1"
+        # convert to shared units
+        era5_ds[var_id] = units.convert_units_to(era5_ds[var_id], units_lu[var_id])
+
+    return era5_ds
+
+
+def extract_era5_time_series(era5_ds, projected_coords):
+    """Extract time series from ERA5 dataset for the given coordinates."""
+    era5_ds = drop_non_coord_vars(era5_ds)
+    return xr.merge(
+        [
+            extract_time_series(era5_ds, var_id, projected_coords)
+            for var_id in era5_ds.data_vars
+        ]
+    )
+
+
+def extract_time_series_from_zarr(zarr_dir, model, scenario, var_id, coords):
+    """Extract time series from zarr stores for ERA5, historical, and future data for a given model and scenario."""
+    # open historical and future data
+    hist_store = zarr_dir.joinpath(f"{var_id}_{model}_historical.zarr")
+    sim_store = zarr_dir.joinpath(f"{var_id}_{model}_{scenario}.zarr")
+    hist_ds = xr.open_zarr(hist_store)
+    sim_ds = xr.open_zarr(sim_store)
+    hist_ds = drop_non_coord_vars(hist_ds)
+    sim_ds = drop_non_coord_vars(sim_ds)
+    # ensure the time coordinates have 0 for the hour
+    hist_ds = hist_ds.assign_coords(time=hist_ds.time.dt.floor("D"))
+    sim_ds = sim_ds.assign_coords(time=sim_ds.time.dt.floor("D"))
+
+    target_unit = units_lu[var_id]
+    # Extract time series for each location
+    hist_extr = units.convert_units_to(
+        extract_time_series(hist_ds, var_id, coords), target_unit
+    )
+    sim_extr = units.convert_units_to(
+        extract_time_series(sim_ds, var_id, coords), target_unit
+    )
+
+    return hist_extr, sim_extr
+
+
+def run_bias_adjustment_and_package_data(
+    zarr_dir, model, scenario, var_id, coords, era5_extr
+):
+    """returns bias adjusted data and non-adjusted data relevant for plotting comparisons.
+    assumes all relevant zarr stores are in the same directory.
+    """
+    hist_extr, sim_extr = extract_time_series_from_zarr(
+        zarr_dir, model, scenario, var_id, coords
+    )
+
+    # need to supply the correct variable to the train function
+    if isinstance(era5_extr, xr.Dataset):
+        era5_extr = era5_extr[var_id]
+
+    # Since it is easy to process both quantile delta mapping and detrended quantile mapping, we will do so
+    train_kwargs = dict(
+        ref=era5_extr,
+        hist=hist_extr,
+        nquantiles=50,
+        group="time.dayofyear",
+        window=31,
+        kind=varid_adj_kind_lu[var_id],
+    )
+    if var_id in adapt_freq_thresh_lu:
+        # do the adapt frequency thingy for precipitation data
+        train_kwargs.update(
+            adapt_freq_thresh=adapt_freq_thresh_lu[var_id],
+            jitter_under_thresh_value=jitter_under_thresh_lu[var_id],
+        )
+
+    dqm_train = sdba.DetrendedQuantileMapping.train(**train_kwargs)
+    qdm_train = sdba.QuantileDeltaMapping.train(**train_kwargs)
+
+    # adjust and combine the historical data
+    hist_adj_da = adjust_and_combine_dqm_qdm(dqm_train, qdm_train, hist_extr)
+    hist_adj_da = (
+        hist_adj_da.assign_coords(
+            experiment="historical",
+            # Method=[m + "-historical" for m in hist_adj_da.Method.values]
+        )
+        .expand_dims(["experiment"])
+        .load()
+    )
+    # do the same for the future data
+    sim_adj_da = adjust_and_combine_dqm_qdm(dqm_train, qdm_train, sim_extr)
+    sim_adj_da = (
+        sim_adj_da.assign_coords(
+            experiment=scenario,
+            # Method=[m + "-future" for m in sim_adj_da.Method.values]
+        )
+        .expand_dims(["experiment"])
+        .load()
+    )
+
+    # addign Method and experiment dims for each of the unadjusted dataarrays
+    hist_extr = (
+        hist_extr.assign_coords(Method=f"{model}", experiment="historical")
+        .expand_dims(["Method", "experiment"])
+        .load()
+    )
+    sim_extr = (
+        sim_extr.assign_coords(Method=f"{model}", experiment=scenario)
+        .expand_dims(["Method", "experiment"])
+        .load()
+    )
+
+    return xr.merge([hist_extr, sim_extr, hist_adj_da, sim_adj_da])[var_id]
+
+
+def run_indicators(da, era5_extr):
+    """Run a set of indicators on the given dataframe."""
+    var_id = da.name
+    # combine in the ERA5 with adjusted data
+    da = xr.concat(
+        [era5_extr[var_id].expand_dims({"Method": ["ERA5"]}), da], dim="Method"
+    )
+
+    indicator_das = []
+    for index in indices_lu[var_id]:
+        indicator_das.append(indices_lu[var_id][index](da))
+
+    indicators = xr.merge(indicator_das)
+    return indicators
+
+
+### plotting functions
+
+
+def plot_kde(df, var_id):
+    """Plots a KDE of the given variable, location, and method."""
+
+    g = sns.displot(
+        data=df,
+        x=var_id,
+        hue="Method",
+        col="location",
+        kind="kde",
+        col_wrap=3,
+        facet_kws={"sharex": False, "sharey": False, "legend_out": False},
+        common_norm=False,
+    )
+
+    sns.move_legend(g, "upper left", bbox_to_anchor=(0.65, 0.45), frameon=False)
+
+    # pr distros have long tails, so limit the x axis to 25% of inital xlimit for better visualization
+    if var_id == "pr":
+        for ax in g.axes.flat:
+            ax.set_xlim(0, ax.get_xlim()[1] * 0.25)
+
+
+def indicator_boxplot_by_method_location(indicators, indicator):
+    """Create a seaborn catplot of an indicator faceted by location and colored by method"""
+    historical_indicators_df = indicators.to_dataframe()
+
+    g = sns.catplot(
+        historical_indicators_df,
+        kind="box",
+        y=indicator,
+        hue="Method",
+        col="location",
+        sharey=False,
+        col_wrap=3,
+        height=2.5,
+    )
+    sns.move_legend(g, "upper left", bbox_to_anchor=(0.75, 0.45), frameon=False)
+    g.figure.suptitle(indicators[indicator].attrs["long_name"])
+    plt.tight_layout()
+    plt.show()
