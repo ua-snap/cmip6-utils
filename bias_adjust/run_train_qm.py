1d8de58b6b52ea9131d095c46d796390e9947b1d
Kyle <kmredilla@alaska.edu>
2025-04-17 17:11:17 -0800

    fix job id printing

--- bias_adjust/run_train_qm.py
+++ bias_adjust/run_train_qm.py
@@ -283,4 +283,4 @@ if __name__ == "__main__":
     sbatch_paths = write_all_sbatch_train_qm(**all_sbatch_kwargs)
 
     job_ids = [submit_sbatch(sbatch_path) for sbatch_path in sbatch_paths]
-    print(job_ids)
+    _ = [print(job_id, end=" ") for job_id in job_ids]
