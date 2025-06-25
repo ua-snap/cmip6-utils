7631b006a5e79aa3636b851f9163a3fe5999c2a2
Kyle <kmredilla@alaska.edu>
2025-06-24 17:02:00 -0800

    split out some tests into separate scripts

---
+++ bias_adjust/tests/test_pr_bounds.py
@@ -0,0 +1,6 @@
+"""Test precipitation bounds in a dataset."""
+
+
+def test_precipitation_bounds(sanity_checker):
+    """Test precipitation data bounds."""
+    sanity_checker.test_pr_bounds()
---
+++ bias_adjust/tests/test_tasmin_bounds.py
@@ -0,0 +1,6 @@
+"""Test minimum temperature bounds in a dataset."""
+
+
+def test_minimum_temperature_bounds(sanity_checker):
+    """Test maximum temperature bounds."""
+    sanity_checker.test_tasmin_bounds()
