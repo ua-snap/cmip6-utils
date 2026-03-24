a1f0635eec44a06279d31e5ffbaef05e93bfb55d
Joshdpaul <jdpaul3@alaska.edu>
2026-03-24 10:40:42 -0800

    add new vars for transfer (daily hurs variables only)

--- transfers/config.py
+++ transfers/config.py
@@ -15,7 +15,8 @@ acdn_ep = "7235217a-be50-46ba-be31-70bffe2b5bf4"
 acdn_prefix = Path("/CMIP6")
 
 # the endpoint for the LLNL ESGF node
-llnl_ep = "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"
+#llnl_ep = "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"
+llnl_ep = "dea29ae8-bb92-4c63-bdbc-260522c92fe8"
 
 # path prefix for LLNL ESGF CMIP6 data
 llnl_prefix = Path("/css03_data/CMIP6")
@@ -123,7 +124,7 @@ model_inst_lu = {
 # lists of models of interest
 # these are all of the models we want to check holdings for
 models_of_interest = [
-    "ACCESS-CM2",
+    #"ACCESS-CM2",
     "CESM2",
     "CNRM-CM6-1-HR",
     "EC-Earth3-Veg",
@@ -136,106 +137,114 @@ models_of_interest = [
     "NorESM2-MM",
     "TaiESM1",
     "MPI-ESM1-2-HR",
-    "CESM2-WACCM",
-    "MPI-ESM1-2-LR",
+    #"CESM2-WACCM",
+    #"MPI-ESM1-2-LR",
 ]
 # E3SM-1-1-NARRM not available on globus according to MetaGrid
-e3sm_models_of_interest = ["E3SM-1-0", "E3SM-1-1", "E3SM-2-0", "E3SM-1-1-ECA"]
+e3sm_models_of_interest = [
+    #"E3SM-1-0", 
+    "E3SM-1-1", 
+    "E3SM-2-0", 
+    #"E3SM-1-1-ECA"
+    ]
 
 # we will just use this dict as the reference for production variables.
 variables = {
-    "tas": {"name": "near_surface_air_temperature", "table_ids": ["Amon", "day"]},
-    "ts": {"name": "surface_temperature", "table_ids": ["Amon", "day"]},
-    "tasmax": {
-        "name": "maximum_near_surface_air_temperature",
-        "table_ids": ["Amon", "day"],
-    },
-    "tasmin": {
-        "name": "minimum_near_surface_air_temperature",
-        "table_ids": ["Amon", "day"],
-    },
-    "pr": {"name": "precipitation", "table_ids": ["Amon", "day"]},
-    "psl": {"name": "sea_level_pressure", "table_ids": ["Amon", "day"]},
-    "huss": {"name": "near_surface_specific humidity", "table_ids": ["Amon", "day"]},
-    "uas": {"name": "near_surface_eastward_wind", "table_ids": ["Amon", "day"]},
-    "vas": {"name": "near_surface_northward_wind", "table_ids": ["Amon", "day"]},
-    "ta": {"name": "air_temperature", "table_ids": ["Amon", "day"]},
-    "ua": {"name": "eastward_wind", "table_ids": ["Amon", "day"]},
-    "va": {"name": "northward_wind", "table_ids": ["Amon", "day"]},
-    "sfcWind": {"name": "near_surface_wind_speed", "table_ids": ["Amon", "day"]},
-    "sfcWindmax": {
-        "name": "maximum_near_surface_wind_speed",
-        "table_ids": ["Amon", "day"],
-    },
-    "hus": {"name": "specific_humidity", "table_ids": ["Amon", "day"]},
-    "evspsbl": {
-        "name": "evaporation_including_sublimation_and_transpiration",
-        "table_ids": ["Amon", "Eday"],
-    },
-    "mrro": {"name": "total_runoff", "table_ids": ["Lmon", "day"]},
-    "mrsos": {
-        "name": "moisture_in_upper_portion_of_soil_column",
-        "table_ids": ["Lmon", "day"],
-    },
-    "mrsol": {
-        "name": "moisture_in_upper_portion_of_soil_column",
-        "table_ids": ["Emon", "Eday"],
-    },
-    "prsn": {
-        "name": "snowfall_flux",
-        "table_ids": ["Amon", "day"],
-    },
-    "snd": {"name": "surface_snow_thickness", "table_ids": ["LImon", "Eday"]},
-    "snw": {"name": "surface_snow_amount", "table_ids": ["LImon", "day"]},
-    "rlds": {
-        "name": "surface_downwelling_longwave_flux_in_air",
-        "table_ids": ["Amon", "day"],
-    },
-    "rlus": {
-        "name": "surface_upwelling_longwave_flux_in_air",
-        "table_ids": ["Amon", "day"],
-    },
-    "rsds": {
-        "name": "surface_downwelling_shortwave_flux_in_air",
-        "table_ids": ["Amon", "day"],
-    },
-    "rsus": {
-        "name": "surface_upwelling_shortwave_flux_in_air",
-        "table_ids": ["Amon", "day"],
-    },
-    "rls": {
-        "name": "surface_net_downward_longwave_flux",
-        "table_ids": ["Emon", "day"],
-    },
-    "rss": {
-        "name": "surface_net_downward_shortwave_flux",
-        "table_ids": ["Emon", "day"],
-    },
-    "orog": {"name": "surface_altitude", "table_ids": ["fx"]},
-    "sftlf": {
-        "name": "percentage_of_the_grid_cell_occupied_by_land_including_lakes",
-        "table_ids": ["fx"],
-    },
-    "sftof": {"name": "sea_area_percentage", "table_ids": ["Ofx"]},
-    "clt": {"name": "cloud_area_fraction", "table_ids": ["Amon", "day", "Eday"]},
-    "tos": {"name": "sea_surface_temperature", "table_ids": ["Omon", "Oday"]},
-    "siconc": {"name": "sea_ice_area_fraction", "table_ids": ["SImon", "SIday"]},
-    # there is also "siconca", which has the same name, but the files are generally much smaller,
-    #  so they are likely a subset os summary in some way of the siconc data
-    "sithick": {"name": "sea_ice_thickness", "table_ids": ["SImon", "SIday"]},
-    "hfls": {
-        "name": "surface_upward_latent_heat_flux",
-        "table_ids": ["Amon", "day", "Eday"],
-    },
-    "hfss": {
-        "name": "surface_upward_sensible_heat_flux",
-        "table_ids": ["Amon", "day", "Eday"],
-    },
-    "tsl": {"name": "soil_temperature", "table_ids": ["Lmon", "Eday"]},
-    "mlotst": {
-        "name": "ocean_mixed_layer_thickness_defined_by_sigma_t",
-        "table_ids": ["Omon", "Eday"],
-    },
+    "hurs": {"name": "relative_humidity", "table_ids": ["day"]},
+    "hursmin": {"name": "minimum_relative_humidity", "table_ids": ["day"]},
+    "hursmax": {"name": "maximum_relative_humidity", "table_ids": ["day"]},
+    # "tas": {"name": "near_surface_air_temperature", "table_ids": ["Amon", "day"]},
+    # "ts": {"name": "surface_temperature", "table_ids": ["Amon", "day"]},
+    # "tasmax": {
+    #     "name": "maximum_near_surface_air_temperature",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "tasmin": {
+    #     "name": "minimum_near_surface_air_temperature",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "pr": {"name": "precipitation", "table_ids": ["Amon", "day"]},
+    # "psl": {"name": "sea_level_pressure", "table_ids": ["Amon", "day"]},
+    # "huss": {"name": "near_surface_specific humidity", "table_ids": ["Amon", "day"]},
+    # "uas": {"name": "near_surface_eastward_wind", "table_ids": ["Amon", "day"]},
+    # "vas": {"name": "near_surface_northward_wind", "table_ids": ["Amon", "day"]},
+    # "ta": {"name": "air_temperature", "table_ids": ["Amon", "day"]},
+    # "ua": {"name": "eastward_wind", "table_ids": ["Amon", "day"]},
+    # "va": {"name": "northward_wind", "table_ids": ["Amon", "day"]},
+    # "sfcWind": {"name": "near_surface_wind_speed", "table_ids": ["Amon", "day"]},
+    # "sfcWindmax": {
+    #     "name": "maximum_near_surface_wind_speed",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "hus": {"name": "specific_humidity", "table_ids": ["Amon", "day"]},
+    # "evspsbl": {
+    #     "name": "evaporation_including_sublimation_and_transpiration",
+    #     "table_ids": ["Amon", "Eday"],
+    # },
+    # "mrro": {"name": "total_runoff", "table_ids": ["Lmon", "day"]},
+    # "mrsos": {
+    #     "name": "moisture_in_upper_portion_of_soil_column",
+    #     "table_ids": ["Lmon", "day"],
+    # },
+    # "mrsol": {
+    #     "name": "moisture_in_upper_portion_of_soil_column",
+    #     "table_ids": ["Emon", "Eday"],
+    # },
+    # "prsn": {
+    #     "name": "snowfall_flux",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "snd": {"name": "surface_snow_thickness", "table_ids": ["LImon", "Eday"]},
+    # "snw": {"name": "surface_snow_amount", "table_ids": ["LImon", "day"]},
+    # "rlds": {
+    #     "name": "surface_downwelling_longwave_flux_in_air",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "rlus": {
+    #     "name": "surface_upwelling_longwave_flux_in_air",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "rsds": {
+    #     "name": "surface_downwelling_shortwave_flux_in_air",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "rsus": {
+    #     "name": "surface_upwelling_shortwave_flux_in_air",
+    #     "table_ids": ["Amon", "day"],
+    # },
+    # "rls": {
+    #     "name": "surface_net_downward_longwave_flux",
+    #     "table_ids": ["Emon", "day"],
+    # },
+    # "rss": {
+    #     "name": "surface_net_downward_shortwave_flux",
+    #     "table_ids": ["Emon", "day"],
+    # },
+    # "orog": {"name": "surface_altitude", "table_ids": ["fx"]},
+    # "sftlf": {
+    #     "name": "percentage_of_the_grid_cell_occupied_by_land_including_lakes",
+    #     "table_ids": ["fx"],
+    # },
+    # "sftof": {"name": "sea_area_percentage", "table_ids": ["Ofx"]},
+    # "clt": {"name": "cloud_area_fraction", "table_ids": ["Amon", "day", "Eday"]},
+    # "tos": {"name": "sea_surface_temperature", "table_ids": ["Omon", "Oday"]},
+    # "siconc": {"name": "sea_ice_area_fraction", "table_ids": ["SImon", "SIday"]},
+    # # there is also "siconca", which has the same name, but the files are generally much smaller,
+    # #  so they are likely a subset os summary in some way of the siconc data
+    # "sithick": {"name": "sea_ice_thickness", "table_ids": ["SImon", "SIday"]},
+    # "hfls": {
+    #     "name": "surface_upward_latent_heat_flux",
+    #     "table_ids": ["Amon", "day", "Eday"],
+    # },
+    # "hfss": {
+    #     "name": "surface_upward_sensible_heat_flux",
+    #     "table_ids": ["Amon", "day", "Eday"],
+    # },
+    # "tsl": {"name": "soil_temperature", "table_ids": ["Lmon", "Eday"]},
+    # "mlotst": {
+    #     "name": "ocean_mixed_layer_thickness_defined_by_sigma_t",
+    #     "table_ids": ["Omon", "Eday"],
+    # },
 }
 
 # This dict is for auditing WRF variables!
